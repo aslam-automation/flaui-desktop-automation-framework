f2666b24cfe841c920792a899cc325004881a6f3
Muhammed Aslam P <45989462+aslam-automation@users.noreply.github.com>
2026-06-10 09:51:38 +0530

    Create LoginTests.cs

---
+++ Tests/LoginTests.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace DesktopAutomation.Tests
+{
+    public class LoginTests
+    {
+        [Test]
+        public void VerifyApplicationLaunch()
+        {
+            Assert.Pass("Application launched successfully");
+        }
+    }
+}
