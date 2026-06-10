f1e461df24088dd366961973569c85c8b2fb77fd
Muhammed Aslam P <45989462+aslam-automation@users.noreply.github.com>
2026-06-10 09:52:08 +0530

    Create LoginPage.cs

---
+++ Pages/LoginPage.cs
@@ -0,0 +1,14 @@
+using FlaUI.Core.AutomationElements;
+
+namespace DesktopAutomation.Pages
+{
+    public class LoginPage
+    {
+        private readonly Window _window;
+
+        public LoginPage(Window window)
+        {
+            _window = window;
+        }
+    }
+}
